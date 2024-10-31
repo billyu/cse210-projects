997ce03f35dcb166cbca6472bcca96e150258d9f
Bill Yu <bill.yu@ischool.com.tw>
2024-10-31 19:10:53 +0800

    Prep 1 finished

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,6 +4,19 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        // Declare variables for first name and last name
+        string firstName;
+        string lastName;
+
+        // Ask user for first name
+        Console.Write("What is your first name?");
+        firstName = Console.ReadLine();
+
+        // Ask user for last name
+        Console.Write("What is your last name?");
+        lastName = Console.ReadLine();
+
+        // Print out the full name using string interpolation
+        Console.WriteLine($"Your name is {lastName}, {firstName} {lastName}.");
     }
 }
